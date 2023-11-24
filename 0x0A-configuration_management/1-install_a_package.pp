888b884a75ee8a2e13bb675730491533702eb124
Mikias Gedlu <addiserp@gmail.com>
2023-11-24 18:47:51 +0300

    0x0A. Configuration management hot fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # Using Puppet, install flask from pip3.
 
-exec { 'install flask v2.1.0':
-  ensure  => '2.1.0',
-  command => '/usr/bin/pip3 install flask',
+package { 'install flask':
+  ensure   => '2.1.0',
+  name     =>  'flask',
+  provider =>  'pip3',
 }
