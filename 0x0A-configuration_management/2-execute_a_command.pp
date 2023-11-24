96426365aea970e2d3814aed7bb309b406cf73f7
Mikias Gedlu <addiserp@gmail.com>
2023-11-24 16:21:27 +0300

    0x0A. Configuration management hot fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
-# execute pkill command to kill bashscript file killmenow
+# Using Puppet, create a manifest that kills a process named killmenow.
+
 exec { 'pkill':
   command => 'pkill -9 -f killmenow',
   path    => ['/usr/bin', '/usr/sbin', '/bin']
