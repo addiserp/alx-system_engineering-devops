98f90ef5dd49c094dd95c43bdc004b9e2daa3990
Mikias Gedlu <addiserp@gmail.com>
2024-01-17 15:02:42 +0300

    0x16-api_advanced 1hotfix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,21 +8,19 @@ import requests
 
 
 def top_ten(subreddit):
-    """
-        will display top 10 hottest posts on a given subreddit.
-    """
+    """It prints the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
-    options = {
+    params = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, params=options,
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
-    rsult = response.json().get("data")
-    [print(c.get("data").get("title")) for c in rsult.get("children")]
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
