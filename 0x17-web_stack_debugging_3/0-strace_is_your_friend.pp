14f1b519d8d600897aa5e2aacb09e15bbfba2a3a
Mikias Gedlu <addiserp@gmail.com>
2024-01-17 18:40:24 +0300

    0x17-web_stack_debugging_3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Will fix the error why Apache is returning a 500 error.
+
+exec { 'fix error':
+  provider => 'shell',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,4 @@
+## ALX Addis Ababa Assignment
+0x17-web_stack_debugging_3
+## Authors
+Mikias Gedlu
