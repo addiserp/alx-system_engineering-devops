d506abe26ad66e3f8005688f300e201301dd21ca
Mikias Gedlu <addiserp@gmail.com>
2024-01-17 14:54:27 +0300

    0x16-api_advanced 0hotfix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,15 +7,12 @@
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """
-        will return the number of subscribers for a given subreddit
-    """
+    """It returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    x = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    sdir = x.get("data", {}).get("subscribers", 0)
-    return sdir
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
